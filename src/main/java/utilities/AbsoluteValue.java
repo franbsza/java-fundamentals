1438871651ba14752e2ff0a97e41db169726498c
franbsza <franciele.bsza@gmail.com>
2023-05-15 21:26:08 -0300

    add AbsoluteValue

---
+++ src/main/java/utilities/AbsoluteValue.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class AbsoluteValue {
+
+    // return absolute value, in other words even the value be negative return positive value
+    public int convert(int numA, int numB){
+
+        int result = Math.abs(numA - numB);
+        return result;
+    }
+}
